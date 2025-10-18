45f869597b481cef5b247122454df98770cdb6cb
Cuong0306 <cuongnpse173621@fpt.edu.vn>
2025-10-18 19:53:02 +0700

    style: fixstructure

--- ElectricVehicleDealer.BLL/Services/Implementations/QuoteService.cs
+++ ElectricVehicleDealer.BLL/Services/Implementations/QuoteService.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ElectricVehicleDealer.BLL.Services.Implementations
+namespace ElectricVehicleDealer.BLL.Services.Interfaces.Implementations
 {
     public class QuoteService : IQuoteService
     {
--- ElectricVehicleDealer.BLL/Services/Implementations/VehicleService.cs
+++ ElectricVehicleDealer.BLL/Services/Implementations/VehicleService.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ElectricVehicleDealer.BLL.Services.Implementations
+namespace ElectricVehicleDealer.BLL.Services.Interfaces.Implementations
 {
     public class VehicleService : IVehicleService
     {
