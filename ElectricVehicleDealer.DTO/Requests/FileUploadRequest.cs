11fc7ab4b40f0004f18396052764cd097fac9e55
nhatduy111003 <phamnhatduy08@gmail.com>
2025-10-24 15:11:06 +0700

    Add Callback Vehicle, Get Vehicle By ID, Set isLocated Vehicle

---
+++ ElectricVehicleDealer.DTO/Requests/FileUploadRequest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricVehicleDealer.DTO.Requests
+{
+    public class FileUploadRequest
+    {
+        public IFormFile File { get; set; } = default!;
+    }
+
+    public class MultiFileUploadRequest
+    {
+        public List<IFormFile> Files { get; set; } = new();
+    }
+
+}
