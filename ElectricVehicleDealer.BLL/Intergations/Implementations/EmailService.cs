a24b0a949f2cd8b510bb83c91684514d96078cfd
Cuong0306 <cuongnpse173621@fpt.edu.vn>
2025-11-10 14:37:02 +0700

    feat: mail

---
+++ ElectricVehicleDealer.BLL/Intergations/Implementations/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using ElectricVehicleDealer.BLL.Intergations.Interfaces;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System.Threading.Tasks;
+
+public class EmailService : IEmailService
+{
+    private readonly IConfiguration _configuration;
+    public EmailService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task SendEmailAsync(string to, string subject, string body)
+    {
+        var emailMessage = new MimeMessage();
+        emailMessage.From.Add(new MailboxAddress(
+            _configuration["EmailSettings:SenderName"],
+            _configuration["EmailSettings:SenderEmail"]));
+        emailMessage.To.Add(MailboxAddress.Parse(to));
+        emailMessage.Subject = subject;
+        emailMessage.Body = new TextPart("html") { Text = body };
+
+        using var client = new SmtpClient();
+        await client.ConnectAsync(
+            _configuration["EmailSettings:SmtpServer"],
+            int.Parse(_configuration["EmailSettings:Port"]),
+            MailKit.Security.SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(
+            _configuration["EmailSettings:Username"],
+            _configuration["EmailSettings:Password"]);
+        await client.SendAsync(emailMessage);
+        await client.DisconnectAsync(true);
+    }
+}
